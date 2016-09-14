7ec5d27fb0cbc6fa2a096c2905ae49c912821ef1
Antoha5001 <plantsvzzombi@mail.ru>
2016-09-14 18:10:13 +0700

    done

--- paralax/Parallax-on-the-Web-DevTips--0.0/js/functions.js
+++ paralax/Parallax-on-the-Web-DevTips--0.0/js/functions.js
@@ -27,6 +27,17 @@ $(document).ready(function () {
 			$('.clothes-pics figure').removeClass('is-showing');
 		}
 		
+		if(wScroll > $('.large-window').offset().top - $(window).height()){
+			$('.large-window').css({
+				'background-position':'center '+(wScroll-$('.large-window').offset().top)+'px'
+			});
+			var opacity = (wScroll - $('.large-window').offset().top+400) / (wScroll/5);
+			$('.window-tint').css({
+				'opacity':opacity
+			});
+			console.log(opacity);
+		}
+		
 	});
 
 });
