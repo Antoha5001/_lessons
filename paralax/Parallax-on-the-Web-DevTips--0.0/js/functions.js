00c31fce317ce55535f7bc217a23534952f58424
Antoha5001 <plantsvzzombi@mail.ru>
2016-09-14 14:18:15 +0700

    add files

---
+++ paralax/Parallax-on-the-Web-DevTips--0.0/js/functions.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+	$(window).scroll(function () {
+		var $this = $(this),
+			wScroll = $this.scrollTop();
+		$('.logo').css({
+			'transform': 'translate(0px, ' + wScroll / 2 + '%)'
+		});
+
+		$('.back-bird').css({
+			'transform': 'translate(0px, ' + wScroll / 4 + '%)'
+		});
+		$('.fore-bird').css({
+			'transform': 'translate(0px, -' + wScroll / 40 + '%)'
+		});
+
+		if (wScroll > $('.clothes-pics').offset().top-($(window).height()/1.2)) {
+			
+			$('.clothes-pics figure').each(function (i) {
+				setTimeout(function () {
+					$('.clothes-pics figure').eq(i).addClass('is-showing');
+				}, 150 * (i+1));
+
+			});
+			
+		}else if(wScroll < $('.clothes-pics').offset().top-($(window).height()/1.2)){
+			$('.clothes-pics figure').removeClass('is-showing');
+		}
+		
+	});
+
+});
